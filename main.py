df95ad4653cf564a36bef7dff03889b96ef123fe
Prince <167101466+pkprajapati7402@users.noreply.github.com>
2025-04-10 09:56:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ CORS(app, resources={r"/api/*": {"origins": "https://audihealth-backend.onrender
 
 if __name__ == "__main__":
     # Use the PORT environment variable provided by Render, default to 5000 if not set
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, use_reloader=True)
