5b051dd0e3a146e8233415129151df99b50e0da3
Prince <167101466+pkprajapati7402@users.noreply.github.com>
2025-04-10 09:32:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from flask_cors import CORS
 load_dotenv()
 
 app = create_app()
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/api/*": {"origins": "https://audihealth-backend.onrender.com"}})
 
 if __name__ == "__main__":
     # Use the PORT environment variable provided by Render, default to 5000 if not set
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, use_reloader=False)
